474e4228fc989fd4888d5ecbab9f8138c6b86781
Amjad-khan-fple <amjad@fpletech.in>
2025-03-03 16:48:35 +0530

    redis port added to ENV

--- src/utils/redis/redisClient.js
+++ src/utils/redis/redisClient.js
@@ -1,15 +1,15 @@
 const redis = require("redis");
-
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL || `redis://localhost:${REDIS_PORT}`,
   socket: {
     reconnectStrategy: (retries) => {
       if (retries > 1) {
         console.error("Redis: Max reconnection attempts reached. Giving up.");
-        return false; 
+        return false;
       }
       console.log(`Redis: Reconnection attempt #${retries}`);
-      return 500; 
+      return 500;
     },
   },
 });
